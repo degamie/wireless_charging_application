5109843541c089bbd958c509af23c81bfd950ae1
Degamie_sign <degamiesign@gmail.com>
2024-11-10 16:44:52 +0530

    Third Commit

---
+++ wireless_Device_axr/src/test/java/com/eureka_wireless_Device_axr/wireless_Device_axr/WirelessDeviceAxrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka_wireless_Device_axr.wireless_Device_axr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WirelessDeviceAxrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
