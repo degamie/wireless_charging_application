ff46b12eaa413aae2d5d91c0b3c0c834b20a70a3
Degamie_sign <106448560+degamie@users.noreply.github.com>
2024-12-19 23:38:28 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/BatteryWireless.java
+++ app/src/main/java/com/example/wirelesschargingapplication/BatteryWireless.java
@@ -1,6 +1,9 @@
 package com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application;
 
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -17,5 +20,16 @@ public class BatteryWireless extends AppCompatActivity {
         setContentView(R.layout.activity_full_screen_main);
         textWifiInfo=(textViewInfo) findViewById(R.id.idText);
         btnInfo=(Button) findViewById(R.id.idBtm);
+    }
+    public void getWifiInfo(View view){
+        WifiManager wifiManager=(WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+        WifiInfo wifiInfo=wifiManager.getConnectionInfo();
+        int ip=WifiInfo.getIpAddress();
+        String macAddress=WifiInfo.getMacAddress();
+        String bssId=WifiInfo.getBSSID();
+        int rssid=wifiInfo.getRssi();
+        String linkSpeed=WifiInfo.getLinkSpeed();
+        int ssid=WifiInfo.getSSID();
+        int networkId=WifiInfo.getNetworkId();
     }
 }
