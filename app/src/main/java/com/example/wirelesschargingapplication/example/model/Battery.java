a163a8b3d0698bca8966cb80e453aed6e25471fc
Degamie_sign <106448560+degamie@users.noreply.github.com>
2024-12-20 16:22:13 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/wirelesschargingapplication/example/model/Battery.java
@@ -0,0 +1,8 @@
+package com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.model;
+public class Battery{
+    public Long batteryId;
+    public String batteryName;
+    public String  wifiId;
+    public String wifiName;
+    public Integer BatteryPercentage ;
+}
