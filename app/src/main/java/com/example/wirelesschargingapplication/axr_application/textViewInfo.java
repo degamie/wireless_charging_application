068b2fc0a16d59ad8f2f62e95d302e8e82d3e178
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-01-02 12:38:43 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/axr_application/textViewInfo.java
+++ app/src/main/java/com/example/wirelesschargingapplication/axr_application/textViewInfo.java
@@ -1,4 +1,19 @@
-//package com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application;
-//
-//public class textViewInfo {
-//}
+package com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.wirelesschargingapplication.R;
+
+public class textViewInfo {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_screen_main);
+        TextView textViewInfo=(textViewInfo) findViewById(R.id.text_view_id);
+
+        textViewInfo.setText(R.string.user_wireless_charging);
+    }
+}
+    //        WirelessChargingText
+
