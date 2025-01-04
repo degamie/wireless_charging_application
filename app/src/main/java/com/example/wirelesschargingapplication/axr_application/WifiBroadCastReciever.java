851b354890ffa429e86650984268d66192c197ff
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-01-04 14:04:37 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/axr_application/WifiBroadCastReciever.java
+++ app/src/main/java/com/example/wirelesschargingapplication/axr_application/WifiBroadCastReciever.java
@@ -3,8 +3,11 @@ package com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.location.GnssAntennaInfo.Listener;
 import android.net.wifi.p2p.WifiP2pManager;
+import android.os.Bundle;
+import android.os.Looper;
 import android.service.credentials.Action;
 
 import com.example.axr_application.MainActivity;
@@ -16,12 +19,26 @@ public class WifiBroadCastReciever extends BroadcastReceiver{
     public WifiP2pManager wifiP2pManager;
     public Channel channel;
     public MainActivity mainActivity;
+    public IntentFilter intentFilter;
+    public Context context;
+
     public final WifiBroadCastReciever listener;
     private void onWifiChangeBroadcastListener(Context context, Intent intent){
         super();
         this.wifiP2pManager=wifiP2pManager;
         this.channel=channel;
         this.mainActivity=mainActivity;
+
+    }
+    public void onCreate(Bundle savedInstanceState){
+        wifiP2pManager=(wifiP2pManager)getSystemSerializable(context.WIFI_SERVICE);
+        channel=wifiP2pManager.initialize(this, Looper.getMainLooper(),null);
+        broadcastReceiver=new WiFiDirectBroadcastReceiver(wifiP2pManager,channel,this);
+
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
 
     }
     @Override
