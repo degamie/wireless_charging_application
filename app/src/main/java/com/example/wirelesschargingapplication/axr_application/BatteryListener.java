3e2e2cf4ceb8ab5b2e599336063f8e025eff35dc
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-01-06 23:53:00 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/axr_application/BatteryListener.java
+++ app/src/main/java/com/example/wirelesschargingapplication/axr_application/BatteryListener.java
@@ -4,9 +4,22 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.util.Log;
 
+import java.util.List;
+
 public class BatteryListener extends BroadcastReceiver {
+    public WifiP2pManager.PeerListListener =new WifiP2pManager.PeerListListener<>();
+    @Override
+    public void onPeersAvailable(WifiP2pDeviceList peersList){
+        List<WifiP2pDevice>refreshedPeeers=peersList.getDeviceList();
+        if(!refreshedPeeers.equals(peersList)){
+            peersList.clear();
+        }
+    }
     @Override
     public final String TAG="BatteryListener";
     public  IntentAction intentAction;
--- app/src/main/java/com/example/wirelesschargingapplication/axr_application/batteryAdapter.java
+++ app/src/main/java/com/example/wirelesschargingapplication/axr_application/batteryAdapter.java
@@ -1,8 +1,11 @@
 package com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application;
 
+import android.app.Notification;
 import android.content.Context;
 import android.content.Intent;
 import android.media.tv.BroadcastInfoResponse;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.service.credentials.Action;
 import android.util.Log;
 
 import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.BatteryAPi;
@@ -15,7 +18,17 @@ public class batteryAdapter extends BroadcastInfoResponse {
 }
 public class batteryAdapter extends BatteryWireless{
     public BatteryWireless batteryWireless;
-    public void onResponse(Context Context, Intent intent){System.out.println(batteryWireless.getWifiInfo(());}
+    public void onResponse(Context Context, Intent intent){
+        Action action=new Action();
+        else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)){
+            if(mManager!=null){
+                mManager.requestPeers(channel, WifiP2pManager.PeerListListener){
+                    Log.d(WifiDirectActivity.TAG("Peers (p2p) is Updated "))
+                }
+            }
+        }
+        System.out.println(batteryWireless.getWifiInfo(());
+    }
 
 }
 //   public void ResponseOutput(){
