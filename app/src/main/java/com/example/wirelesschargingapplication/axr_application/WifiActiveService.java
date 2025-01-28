807cce51d18579b53a53ee74b481e92a4d505ee1
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-01-28 23:49:30 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/axr_application/WifiActiveService.java
+++ app/src/main/java/com/example/wirelesschargingapplication/axr_application/WifiActiveService.java
@@ -1,5 +1,7 @@
 package com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application;
 
+import static android.content.ContentValues.TAG;
+
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -10,17 +12,20 @@ import android.util.Log;
 import java.util.logging.Handler;
 
 public class WifiActiveService  extends Service {
-    public final String TAG=WifiActiveService.class.getSimpleName();
-    String onStartCommand(Intent intent,int flag,int StartId){
-        WifiManager wifiManager=(WifiManager) getSystemService(Context.WIFI_SERVICE);
+    public final String TAG = WifiActiveService.class.getSimpleName();
+    public String onStartCommand(Intent intent, int flag, int StartId) {
+        WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         new Handler().postDelayed(new Runnable());
+        if(wifiManager==null)return null;
+        else if(StartId!=0)return wifiManager.Builder().setIsEnhancedOpening(EnhancedOpen);
+        else return StartId;
     }
+}
     public void run(){
         WifiInfo wifiInfo=WifiManager.getConnectionInfo();
         String mac= wifiInfo.getMacAddress();
-        String ssid=WifiInfo.getssid();
+        String ssid=WifiInfo.getSsid();
         if(Log.isLoggable(TAG,Log.VERBOSE)){
             Log.v(TAG,"The SSID and Mac Message are available "+ssid+mac);
         }
     }
-}
