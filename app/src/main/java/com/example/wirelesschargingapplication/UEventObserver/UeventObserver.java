59198b75b7405161b880ba5cc7e86db7e0a7cdeb
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-06 19:05:29 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/UEventObserver/UeventObserver.java
+++ app/src/main/java/com/example/wirelesschargingapplication/UEventObserver/UeventObserver.java
@@ -1,4 +1,14 @@
 package com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BatterySimulator.UEventObserver;
 
 public class UeventObserver {
+    public UeventObserver ueventObserver;
+    public UeventObserver(){
+        this.ueventObserver=ueventObserver;
+    }
+    public final static native  nativeAdd();
+    public final static native  nativeWaitForNextEvent();
+
+    public final static native  nativeSetup();
+    public final static native  nativeRemove();
+
 }
