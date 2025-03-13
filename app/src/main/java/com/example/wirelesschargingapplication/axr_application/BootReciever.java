c09599aa8bdca69f380098b53f3a95bebdda4e7e
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-13 07:35:07 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/axr_application/BootReciever.java
+++ app/src/main/java/com/example/wirelesschargingapplication/axr_application/BootReciever.java
@@ -2,8 +2,19 @@ package com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main
 
 import java.io.File;
 
-public class BootReciever extends BroadCastReciever{
-    public static final String TAG="Boot Reciever";
-    public static final int LOG_SIZE=65336;
-    public static final File TOMBSTONE_DIR=new File("/data/tombStones");
+public class BootReciever extends BroadCastReciever{//BootReciever  Declare Inheriting BroadCastReciever  Class
+    public BootReciever bootReciever;//Obj Declare
+    public static final String TAG="Boot Reciever";//Tag Declare
+    public static final int LOG_SIZE=65336;//Log Size Declare
+    public static final File TOMBSTONE_DIR=new File("/data/tombStones");//TOMBSTONE_DIR declare
+    public BootReciever(){//Default Constructor Declare
+        return;
+    }
+    public BootReciever(String TAG,int LOG_SIZE){//Parameterized Constructor declare
+        this.bootReciever=bootReciever;
+        this.TAG=TAG;
+        this.LOG_SIZE=LOG_SIZE;
+    }
+
+
 }
