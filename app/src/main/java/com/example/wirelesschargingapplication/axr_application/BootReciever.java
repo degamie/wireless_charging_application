c21e6b34ce9b2e3c04373a85f9fc28e907e9ab49
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-16 08:13:31 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/axr_application/BootReciever.java
+++ app/src/main/java/com/example/wirelesschargingapplication/axr_application/BootReciever.java
@@ -1,5 +1,8 @@
 package com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application;
 
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+
 import java.io.File;
 
 //public class BootReciever extends BroadCastReciever{//BootReciever  Declare Inheriting Broadpublic class BootReciever extends BroadCastReciever{//BootReciever  Declare Inheriting BroadCastReciever  ClassCastReciever  Class
@@ -11,6 +14,10 @@ public String TAG=null;//Tag Declare
     public static final File TOMBSTONE_DIR=new File("/data/tombStones");//TOMBSTONE_DIR declare
     public BootReciever(){//Default Constructor Declare
         return;
+    }
+    @SuppressLint("NewApi")
+    public void OnCreate(){//Func declare
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectUnsafeIntentLaunch().build());//Setting Bootstrapping's Security
     }
     public BootReciever(String TAG,int LOG_SIZE,Boolean B){//Parameterized Constructor declare
         this.bootReciever=bootReciever;
