df3b567795eec23084e0f861537d11041fd86e6b
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-29 23:15:58 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/wirelesschargingapplication/StatusIntent.java
@@ -0,0 +1,11 @@
+package com.example.wirelesschargingapplication;
+
+import android.content.Intent;
+
+public class StatusIntent  extends Intent {
+    public StatusIntent statusIntent;
+    public StatusIntent(StatusIntent statusintent){
+        this.statusIntent=statusintent;
+    }
+
+}
