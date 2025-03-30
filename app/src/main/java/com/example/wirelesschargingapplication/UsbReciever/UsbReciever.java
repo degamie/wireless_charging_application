9bae9062596526c621545709fe3ca9b779dc84e1
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-30 13:59:50 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/UsbReciever/UsbReciever.java
+++ app/src/main/java/com/example/wirelesschargingapplication/UsbReciever/UsbReciever.java
@@ -6,6 +6,7 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
 import android.util.Log;
 
@@ -23,6 +24,7 @@ public class UsbReciever extends BroadCastReciever {//UsbReciever Class Declare
     public String CAMERA_PID=null;
     //UsbManager Obect Declare
     public UsbManager usbManager;
+    public UsbDeviceConnection usbDeviceConnection;
     @SuppressLint("LogNotTimber")
     public static final String TAG="UsbReciever";//TAG Declare
     public void findSerialPortDevice() {//FindSerialPort Device Declare
@@ -32,7 +34,12 @@ public class UsbReciever extends BroadCastReciever {//UsbReciever Class Declare
             device = hold;//device's value declare
             int devicePID = device.getProductId();//product Id Declare
             int deviceVID = device.getVendorId();//VendorId Declare
-        }
+            UsbDeviceConnection usbFileDescriptor=usbDeviceConnection.getFileDescriptor();
+            if(devicePID==0)return 0;
+            while(devicePID!=0){
+                else if(devicePID>=deviceVID)devicePID+=usbFileDescriptor;
+                else devicePID=deviceVID;
+        }return devicePID;
     }
     public void onReciever(Context context, Intent intent){//ONReciever Mehtod Declare
         Log.d(TAG,"IntentFilter");//IntentFilter Declare
