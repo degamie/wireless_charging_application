dbde90e32e71c4927be3b2dd22c95c7d33284e1f
Degamie_sign <degamiesign@gmail.com>
2025-04-18 08:56:07 +0530

    Twelth Commit

--- app/src/main/java/com/example/wirelesschargingapplication/Reciever.java
+++ app/src/main/java/com/example/wirelesschargingapplication/Reciever.java
@@ -1,5 +1,13 @@
 package com.example.wirelesschargingapplication;
 
-public class Reciever {
+import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BatteryReciever;
+
+public class Reciever extends BatteryReciever {
     public Reciever reciever;
+    public Reciever(Reciever reciever){
+        this.reciever=reciever;
+    }
+    public String getReciever(Reciever reciever){
+        return reciever;
+    }
 }
