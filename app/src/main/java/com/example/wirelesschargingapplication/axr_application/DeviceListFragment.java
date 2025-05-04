67dd52ae0f6c00ca64f108650f1c5692cebdd5c5
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-05-04 08:32:04 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/wirelesschargingapplication/axr_application/DeviceListFragment.java
@@ -0,0 +1,15 @@
+package com.example.wirelesschargingapplication;
+public class DeviceListFragment {
+    public DevicelistFragment devicelistframagment;
+    public String deviceName=null;
+    public String serialId=null;
+    public int portNum=0;
+
+    public DeviceListFragment(String deviceName,String serialId,int portNum){
+        this.deviceName=deviceName;
+        this.serialId=serialId;
+        this.portNum=portNum;
+    }
+
+
+}
