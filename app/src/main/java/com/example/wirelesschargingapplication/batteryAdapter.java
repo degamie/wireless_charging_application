5e2db5cbda2d3921a67091356356ab8472970b6a
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-06-01 10:43:38 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/wirelesschargingapplication/batteryAdapter.java
@@ -0,0 +1,38 @@
+package com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application;
+
+import android.app.Notification;
+import android.content.Context;
+import android.content.Intent;
+import android.media.tv.BroadcastInfoResponse;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.service.credentials.Action;
+import android.util.Log;
+
+import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.BatteryAPi;
+
+public class batteryAdapter extends BroadcastInfoResponse {
+    public String getContext(Context context){
+        return context;
+    }
+    public void onStart()(){
+        Log.i(BatteryAPi.class.getresponse());
+        System.out.println("Battery Adapter Connected"+);}
+}
+public class batteryAdapter extends BatteryWireless{
+    public BatteryWireless batteryWireless;
+    public void onResponse(Context Context, Intent intent){
+        Action action=new Action();
+        else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)){
+            if(mManager!=null){
+                mManager.requestPeers(channel, WifiP2pManager.PeerListListener){
+                    Log.d(WifiDirectActivity.TAG("Peers (p2p) is Updated "))
+                }
+            }
+        }
+        System.out.println(batteryWireless.getWifiInfo(());
+    }
+
+}
+//   public void ResponseOutput(){
+//
+//   }
