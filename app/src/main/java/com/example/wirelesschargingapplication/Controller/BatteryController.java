c2674528b3f8e29b4e740e83f5a0acf7148702b9
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-06-17 09:50:03 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/Controller/BatteryController.java
+++ app/src/main/java/com/example/wirelesschargingapplication/Controller/BatteryController.java
@@ -1,11 +1,16 @@
 @Controller
 public class BatteryController{//BatteryController Class Declare
+    public BatteryController batteryController;
     @Autowired
     public BatteryService batteryService;//Injecting batteryService implement
     @GetMapping("/save/{batteryid}")//Getting Batterid's Data
     public String getAllBatteryById(String BatteryId){//getAllBatteryById Method
         return batteryService.getAllBatteryById(BatteryId);//fetching BatteryId
     }
+    @GetMapping("/save/{batteryName}")//Getting BatterName's Data
+    public String getAllByBatteryName(String BatteryName){
+        return batteryService.getAllByBatteryName(BatteryName);
 
+    }
 }
 //    @Override
