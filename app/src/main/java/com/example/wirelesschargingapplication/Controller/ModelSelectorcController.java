e97c0b95b031244c329e2373dd5d55d29b6f657d
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-06-21 14:39:48 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/wirelesschargingapplication/Controller/ModelSelectorcController.java
@@ -0,0 +1,26 @@
+package com.example.axremulator2.ModelSelector;
+
+import android.content.Context;
+
+import androidx.compose.runtime.Anchor;
+
+import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.Camera;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.example.axremulator2.ModelSelector.XRSession;
+@Controller
+public class ModelSelectorController{//ModelSelectorController clss declare
+@Autowired
+public ModelSelectorService modelSelectorService;//modelSelectorService Inject Lib
+    @GetMapping("/save/{arcam}")
+    public String getAllCameraSelector(Camera arcam){//fetching All ArCamera Selecgtor
+        return modelSelectorService.saveALlCameraSelector(arcam);
+    }
+
+}
+//        TransformableModel transformableModel=new TransformableModel(arCam.getTransformationSystem());
+//        transformableModel.setParent(anchorNode);
+//        transformableModel.setRenderable(modelRenderable);
+//        transformableModel.select();
+
