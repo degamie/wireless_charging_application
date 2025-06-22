188eba8e32a6356aeca4a703189857250ca6f468
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-06-22 09:21:57 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/BatteryRecieverRepository.java
@@ -0,0 +1,20 @@
+package com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application;
+
+import static androidx.compose.ui.semantics.SemanticsPropertiesKt.setText;
+
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.BatteryState;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.TextView;
+
+import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BatterySimulator.BatteryWireless;
+import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BroadCastReciever;
+
+@Repository
+public interface  BatteryRecieverRepository  extends JpaRepository<BatteryReciever,String> {//BatteryReciever class inherititing BroadCastReciever
+     @Autowired
+     public com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BatteryReciever BatteryReciver;
+     public String saveAllByBatteryLevel(Integer mBatteryLevel);
+    }
