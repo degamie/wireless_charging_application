982a6f721e938170e90096dfd064593f6dfd8569
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-06-26 09:19:08 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/BatteryRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/BatteryRepository.java
@@ -1,5 +1,6 @@
 @Repository
 public interface BatteryRepository extends JpaRepository<Battery,String>{
+    public String saveAllBWifiyId(long  WifiId);
     public String saveAllByBatteryId(String BatteryId);
     public String saveAllByBatteryName(String BatteryName);
 }
