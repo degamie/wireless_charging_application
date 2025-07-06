83414c014e8abcbabcdc8416e18e8281ed6bda50
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-07-06 15:10:26 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/BatteryWirelessRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/BatteryWirelessRepository.java
@@ -1,7 +1,7 @@
-package com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BatterySimulator;
+package com.example.wirelesschargingapplication.repository;
+
 import static android.content.Context.USB_SERVICE;
 import static android.provider.SyncStateContract.Helpers.update;
-
 import static androidx.core.content.ContextCompat.getSystemService;
 import static androidx.core.content.ContextCompat.registerReceiver;
 
@@ -16,86 +16,20 @@ import android.os.BatteryManager;
 import android.os.DropBoxManager;
 import android.provider.Settings;
 
-//import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.wirelesschargingapplication.MainActivity;
-//import com.example.wirelesschargingapplication.R;
+import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BatterySimulator.LED;
 import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BatterySimulator.UEventObserver.UeventObserver;
 import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BatterySimulator.UsbReciever.UsbReciever;
-import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.wifi.WifiBroadCastReciever;
-import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.wifi.WifiDetails;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BatterySimulator.LED;
+import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BatterySimulator.BatteryWireless;
+
 //public class BatteryWireless extends AppCompatActivity implements WifiBroadCastReciever.wifiChangeBroadCastLister{
 @Repository
-public interface BatteryWirelessRepository extends JpaRepository<BatteryWireless,String> {
-    @Autowired
+public interface BatteryWirelessRepository extends JpaRepository  <BatteryWireless,String>{
+    @Autowwired
     public BatteryWireless batterywireless;
-    public List<BatteryWireless> saveAll();
-    public String saveAllByBattteryState(BatteryState batteryState);
-    public String saveAllByIntent(Intent);
-
+    public String saveAllByMplugType(Integer mPLugType);
 
 }
-
-//    public BatteryState batteryState;
-//    public BatteryWireless batteryWireless = new BatteryWireless();
-//    public View view;
-//
-//    public void getWifiInfo(View view) {
-//        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
-//        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-//        int ip = WifiInfo.getIpAddress();
-//        String macAddress = WifiInfo.getMacAddress();
-//        String bssId = WifiInfo.getBSSID();
-//        int rssid = wifiInfo.getRssi();
-//        String linkSpeed = WifiInfo.getLinkSpeed();
-//        int ssid = WifiInfo.getSSID();
-//        int networkId = WifiInfo.getNetworkId();
-//    }
-//}
-//public class BatteryWireless extends AppCompatActivity {
-//    public BatteryWireless batteryWireless;
-//    public WifiBroadCastReciever wifiBroadCastReciever;
-//    public WifiManager wifiManager;
-//    public TextView textWifiInfo;
-//    public Button btnInfo;
-//}
-//    public class BatteryWireless extends WifiDetails{
-//        public Boolean iswifiVal=false;
-//        public BatteryWireless batteryWireless;
-//        public WifiDetails wifiDetails=new WifiDetails();
-//
-//        @Override
-//        public String setWifiDetails(BatteryState batteryState) {
-//            batteryState.getCapacity();
-//            WifiManager.WIFI_STATE_ENABLED;
-//
-//            super.setWifiDetails();
-//            if(wifiDetails==wifiDetails.getAllWifiDetails()){
-//                iswifiVal=true;
-//            }else iswifiVal=false;
-//        }return iswifiVal ;
-//    }
-//
-//public class BatteryWireless extends WifiDetails {
-//    public Boolean iswifiVal=false;
-//    public BatteryWireless batteryWireless;
-//    public WifiDetails wifiDetails=new WifiDetails();
-//
-//    @Override
-//    public String setWifiDetails() {
-//        super.setWifiDetails();
-//        if(wifiDetails==wifiDetails.getAllWifiDetails()){
-//            iswifiVal=true;
-//        }else iswifiVal=false;
-//    }return iswifiVal ;
-//}
-//public class BatteryWireless extends WifiBroadCastReciever{
-//
-//    public void onReponse(Context context, Intent intent,View view){
-//
-//        System.out.println("Response Connected",MainActivity.getWifiInfo(view));
-//    }
