713c60957ace7093b107645361a1f1a7c40ad283
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-07-22 08:03:47 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/BatteryRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/BatteryRepository.java
@@ -1,7 +1,13 @@
+package  com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.Repository.BatteryRepository;
+
+import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.model.Battery;
 @Repository
 public interface BatteryRepository extends JpaRepository<Battery,String>{
     public String findAllByWifiyId(long  WifiId);
     public String saveAllBWifiyId(long  WifiId);
     public String saveAllByBatteryId(String BatteryId);
     public String saveAllByBatteryName(String BatteryName);
+    public String saveAllByBatteryPercentage(String BatteryPercentage);
+
+
 }
