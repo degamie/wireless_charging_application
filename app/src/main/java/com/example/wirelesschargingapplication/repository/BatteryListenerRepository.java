2856a3f62c3e542075ff1d512a5b8b94d793d9fa
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-08-02 21:44:05 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/BatteryListenerRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/BatteryListenerRepository.java
@@ -4,7 +4,10 @@ import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.
 public interface BatteryListenerRepository extends JpaRepository<batterylistner,String>{
     @Autowired
     public BatteryListener batterylistner;
+    public String saveAllByStatus(String Status);
     public  List<BatteryListener> saveAllByContext(String BatteryId);
     public List<BatteryListener> saveAll(String BatteryId);
     public  List<BatteryListener> saveAllById(String BatteryId);
+
+    public List<BatteryListener>findAllByBatteryId(String BatteryId);
 }
