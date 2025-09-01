355e2ac6ec1a8e94b79d47da6c5d86296236f7f6
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-09-01 06:10:41 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/Controller/UsbRecieverController.java
+++ app/src/main/java/com/example/wirelesschargingapplication/Controller/UsbRecieverController.java
@@ -8,8 +8,11 @@ public class UsbRecieverController{//Class Declare
    public  String getProudctId(@PathVariable "ProductId" String ProductId,@RequestBody UsbReciever){//Fetching ProductId
        return usbRecieverService.getProudctId(ProductId);
    }
-    @PostMapping("/findAll/{ProductId}")//ProductId's Retrieval in Server
+    @PostMapping("/findAll/{ProductId}")
     public String setAllByProductId(String ProductId){
-        return usbRecieverRepository.findAllByProductId(ProductId);
+        return usbRecieverService.findAllByProductId(ProductId);
     }
-}
+    @GetMapping("/saveAll/{VendorID}")
+    public String getAllVendorID(String VendorID){
+        return  usbRecieverService.getAllVendorID(VendorID);//Fetching VendorId in Server
+    }
