d27411c44e7e236aaba27aad1b6be2d75fbeb2d9
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-09-06 16:38:10 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/Controller/BatteryListenerController.java
+++ app/src/main/java/com/example/wirelesschargingapplication/Controller/BatteryListenerController.java
@@ -1,20 +1,17 @@
 import javax.naming.Context;
 @Controller
-public class BatteryListenerController{
-@Autowired
- public BattteryListenerService battteryListenerService;
+public class BatteryListenerController {
+    @Autowired
+    public BattteryListenerService battteryListenerService;
 
-@GetMapping("/save/{context}")
-    public String getAllBatteryContext(@RequestBody Context context){
+    @GetMapping("/save/{context}")
+    public String getAllBatteryContext(@RequestBody Context context) {
 
-    return battteryListenerService.getAllBatteryContext(context);
+        return battteryListenerService.getAllBatteryContext(context);
     }
-    @PostMapping("/findAll/{BatteryId}")//retriving BatteryId in Server
-    public String setAllBatteryId(String BatteryId){
-        return batteryRepository.findAllByBatteryId(BatteryId);
-    }
-    @GetMapping("/saveAll/{Status}")//Fetching All Status In Battery
-    public String getAllByStatus(String Status){
-        return batteryListnerRepository.saveAllByStatus(Status );
+    @GetMapping("/save/{BatteryId}")
+    public String setAllById(String BatteryId) {
+        return batteryRepository.findAllById(Id);
     }
 
+}
