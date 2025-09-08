f9d11697be8fcffce750ecf5e29b418744db3afa
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-09-08 23:32:23 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/service/BatteryListenerService.java
+++ app/src/main/java/com/example/wirelesschargingapplication/service/BatteryListenerService.java
@@ -4,6 +4,7 @@ import javax.naming.Context;
 public class BatteryListenerService {//BatteryListenerService class declare
     @Autowired
     public BatteryListnerRepository batteryRepository;//batteryRepository's injection
+    public String  getAllById(String BatteryId){return batteryRepository.saveAllById(BatteryId);
       public BatteryListenerService battteryListenerService=new BatteryListenerService();//Obj declare
       public String getAllContext(Context context{//Fetching Context
           return batteryRepository.saveAll(context);
