4a90b78400089ba45f9bbfcad64217f50946a46e
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-09-10 09:46:46 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/UsbRecieverRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/UsbRecieverRepository.java
@@ -4,4 +4,5 @@ public interface UsbRecieverRepository extends JpaRepository<UsbReciever,String>
     public  String getProudctId(String ProductId);
     public String findAllByProductId(String ProductId);
     public String getAllVendorID(String VendorID);
+    public String findAllByVendorId(String VendorID);//only in repo Changes//Impl TBC//
 }
