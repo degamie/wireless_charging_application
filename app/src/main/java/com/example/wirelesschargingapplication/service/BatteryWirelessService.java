b41f3bf8894c274ea69714ad8d1c29d16462df75
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-09-13 19:56:48 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/service/BatteryWirelessService.java
+++ app/src/main/java/com/example/wirelesschargingapplication/service/BatteryWirelessService.java
@@ -27,6 +27,9 @@ public class BatteryWirelessService {
     public String findAllByMplugType(Integer mPLugType){
         return batterywirelessRepository.findAllByMplugType(mPLugType);
     }
+    public String setAllBYScale(int Scale){
+         return batterywirelessRepository.findAllBYScale(Scale);
+    }
 
 
 }
