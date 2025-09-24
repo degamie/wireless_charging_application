22950c018c3c3a5c959cf12724bf961a3efd0db3
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-09-24 06:37:29 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/Controller/DeviceListFragmentController.java
+++ app/src/main/java/com/example/wirelesschargingapplication/Controller/DeviceListFragmentController.java
@@ -1,1 +1,10 @@
-public class DeviceListController{//TBC}
+@Controller
+public class DeviceListController{
+    @Service
+    public DeviceListFragmentService deviceListService;
+    @PostMapping("/findAll/{SerailId}")
+    public String setAlldserialById(String serialId){
+        return  deviceListService.findAlldserialById(serialId);
+    }
+
+}
