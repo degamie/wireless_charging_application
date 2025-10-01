c02397ad30dc84c38943572fb2bbff0a8977bf59
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-10-01 11:16:52 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/Controller/UsbRecieverController.java
+++ app/src/main/java/com/example/wirelesschargingapplication/Controller/UsbRecieverController.java
@@ -1,21 +1,33 @@
 @Component
 @Controller
 
-public class UsbRecieverController{//Class Declare
+public class UsbRecieverController {//Class Declare
     @Autowired
     public UsbRecieverService usbRecieverService;//Injecting usbRecieverService's Lib
-   @GetMapping("/save/{ProductId}")
-   public  String getProudctId(@PathVariable "ProductId" String ProductId,@RequestBody UsbReciever){//Fetching ProductId
-       return usbRecieverService.getProudctId(ProductId);
-   }
+
+    @GetMapping("/save/{ProductId}")
+    public String getProudctId(@PathVariable "ProductId"String ProductId, @RequestBody UsbReciever) {//Fetching ProductId
+        return usbRecieverService.getProudctId(ProductId);
+    }
+
     @PostMapping("/findAll/{ProductId}")
-    public String setAllByProductId(String ProductId){
+    public String setAllByProductId(String ProductId) {
         return usbRecieverService.findAllByProductId(ProductId);
     }
+
     @GetMapping("/saveAll/{VendorID}")
-    public String getAllVendorID(String VendorID){
-        return  usbRecieverService.getAllVendorID(VendorID);//Fetching VendorId in Server
+    public String getAllVendorID(String VendorID) {
+        return usbRecieverService.getAllVendorID(VendorID);//Fetching VendorId in Server
     }
+
     @GetMapping("/saveAll/{CameraVID}")
-    public String getAllByCameraVID(String CameraVID){return usbRecieverRepository.saveAllByCameraVID(CameraVID);}
+    public String getAllByCameraVID(String CameraVID) {
+        return usbRecieverRepository.saveAllByCameraVID(CameraVID);
+    }
+    @GetMapping("/saveAll/{device}")
+    public String getAllByDevice(Device device) {
+        return usbRecieverRepository.saveAllByDevice(device);
+    }
+}
+
 
