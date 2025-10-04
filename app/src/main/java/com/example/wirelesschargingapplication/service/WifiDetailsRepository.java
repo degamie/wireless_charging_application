67bf347a2d15d3d75ca28b7839bda4e38f63e391
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-10-04 09:04:50 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/wirelesschargingapplication/service/WifiDetailsRepository.java
@@ -0,0 +1,15 @@
+package com.example.wirelesschargingapplication.repository;
+
+import android.net.wifi.WifiInfo;
+
+import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BatterySimulator.BatteryWireless;
+
+@Repository
+public interface WifiDetailsRepository extends jpaRepository <WifiDetailsRepository,String>{
+    public String saveAllByWifiId(String WifiId);
+    public String findAllByWifiID(String WifiID);
+    public String saveAllByWifiName(String WifiName);
+    public String findAllByWIfiName(String WifiName);
+public String saveAllBymaAddress(String mAcAddress);
+public saveAllByipv6Address(String mAcAddress);
+}
