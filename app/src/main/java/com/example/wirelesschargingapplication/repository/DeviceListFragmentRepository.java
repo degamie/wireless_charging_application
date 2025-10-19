22012ac92aff94f670f5292f76ce52a32cd971dd
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-10-19 23:40:51 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/DeviceListFragmentRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/DeviceListFragmentRepository.java
@@ -9,4 +9,5 @@ public interface DeviceListRepsitory extends JpaRepository<DeviceListFragment,St
     public String findAlldserialById(String serialId);
     public String saveAlldserialById(String serialId);
     public String saveAllByDeviceName(String DeviceName);
+    public String saveAllByPort(String Port);
 }
