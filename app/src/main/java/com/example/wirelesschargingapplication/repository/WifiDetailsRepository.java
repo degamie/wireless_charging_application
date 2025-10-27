431c91de1ad0576d3c545ff716e7326219f0b9c7
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-10-27 10:18:04 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/WifiDetailsRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/WifiDetailsRepository.java
@@ -1,5 +1,7 @@
+//WID(27/10/2025)//Degamiesign
 @Repository
 public interface WifiDetailsRepository extends JpaRepository<WifiDetails,String>{
+    public void findAllByNetworkSpeed(double networkSpeed);
 public double saveAllByNetworkSpeed(double networkSpeed);
 
 }
