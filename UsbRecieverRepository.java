ab837525e069a3be0fa1353036ed9da4f4210973
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-11-02 10:54:21 +0530

    Add files via upload

---
+++ UsbRecieverRepository.java
@@ -0,0 +1,17 @@
+import android.health.connect.datatypes.Device;
+
+import javax.naming.Context;
+
+@Repository
+public interface UsbRecieverRepository extends JpaRepository<UsbReciever,String> {
+    public List<UsbReciever> saveAll();
+    public String saveAllByContext(Context context);//Fetching Context In UsbReciever
+    public  String getProudctId(String ProductId);
+    public String findAllByProductId(String ProductId);
+    public String getAllVendorID(String VendorID);
+    public String findAllByVendorId(String VendorID);//only in repo Changes//Impl TBC//
+    public String saveAllByCameraVID(String CameraVID);
+    public String saveAllByDevice(Device device);
+    public String saveAllByCameraPID(String CameraPID);
+    public String findAllByCameraPID(String CameraPID);
+}
