8b898ee8d40f58ed03d2e344feb70ec69639c638
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-11-08 01:58:21 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/UsbRecieverRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/UsbRecieverRepository.java
@@ -1,13 +1,7 @@
-import android.health.connect.datatypes.Device;
-
 @Repository
 public interface UsbRecieverRepository extends JpaRepository<UsbReciever,String> {
     public List<UsbReciever> saveAll();
     public  String getProudctId(String ProductId);
     public String findAllByProductId(String ProductId);
-    public String getAllVendorID(String VendorID);
-    public String findAllByVendorId(String VendorID);//only in repo Changes//Impl TBC//
     public String saveAllByCameraVID(String CameraVID);
-    public String saveAllByDevice(Device device);
-    public String saveAllByCameraPID(String CameraPID);
 }
