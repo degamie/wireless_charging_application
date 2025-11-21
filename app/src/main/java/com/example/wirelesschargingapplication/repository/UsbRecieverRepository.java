9e98eeab00ef085d65acda45d841b5a0d67a4bf1
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-11-21 20:43:20 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/UsbRecieverRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/UsbRecieverRepository.java
@@ -1,18 +1,12 @@
-import android.health.connect.datatypes.Device;
+import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BatterySimulator.UsbReciever.UsbReciever;
 
-import javax.naming.Context;
+import java.util.List;
 
 @Repository
 public interface UsbRecieverRepository extends JpaRepository<UsbReciever,String> {
     public List<UsbReciever> saveAll();
-    public String saveAllByContext(Context context);//Fetching Context In UsbReciever
-    public  String getProudctId(String ProductId);
-    public String findAllByProductId(String ProductId);
-    public String getAllVendorID(String VendorID);
-    public String findAllByVendorId(String VendorID);//only in repo Changes//Impl TBC//
-    public String saveAllByCameraVID(String CameraVID);
-    public void findAllByCameraVID(String CameraVID);//Binding CameraVID IN Server
-    public String saveAllByDevice(Device device);
-    public String saveAllByCameraPID(String CameraPID);
-    public String findAllByCameraPID(String CameraPID);
+    public void findAllByProductId(Long ProductId);//binding ProductiD in it
+    public  String getProudctId(Long ProductId);
+    public String saveAllByProductName(String ProductName);
+
 }
