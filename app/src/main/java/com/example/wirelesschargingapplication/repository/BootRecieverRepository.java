df43c3b9f128cbb5d88e000563ea30367790b944
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-12-16 11:52:13 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/BootRecieverRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/BootRecieverRepository.java
@@ -1,5 +1,6 @@
 package com.example.wirelesschargingapplication.repository;
 
+//WID(13/10/2025)
 
 import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BootReciever;
 @Repository
@@ -8,6 +9,10 @@ public  interface  BootRecieverRepository extends JpaRepsoitory<BootReciever,Str
  public String saveAllByLogSize(Integer LOG_SIZE);
  public String saveAllByTag(String Tag);
  public String findAllByTag(String Tag);
- public void findAllByTOMBDIR(String TOMBDIR);
+ public Strign saveAllByFIle(String File);
+ public Strign findAllByFIle(String File);
+    public String findAllByTOMB_DIR(String TOMB_DIR);//Finding TOMB_DIR
+ public String saveAllByTOMB_DIR(String TOMB_DIR);
+ public String updaateByTOMB_DIR(String TOMB_DIR);
 
 }
