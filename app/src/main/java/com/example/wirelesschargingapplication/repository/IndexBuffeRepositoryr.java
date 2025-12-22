a7d00a36b6a668618bc8de315c0e297adb095fc0
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-12-22 10:41:03 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/IndexBuffeRepositoryr.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/IndexBuffeRepositoryr.java
@@ -3,12 +3,16 @@ import static com.example.axremulator2.MainAxr.New_Implement.Repository.GpuBuffe
 
 import com.example.axremulator2.Common.helpers.IndexBuffer;
 
+import jdk.javadoc.internal.doclets.toolkit.util.IndexBuilder;
+//WID (22/12/2025)
+
 @Repository
 public interface IndexBufferRepository extends JpaRepository<IndexBuffer,String> {
-    public String findAllByIndexBufferId(String IndexBufferId);
     public String saveAllByBufferSize(Integer bufferSize);
     public String findAllByBufferSize(Integer bufferSize);//Finding BufferSize
     public String saveAllByIndexNumber(String BufferNumber);
-    public String findAllByIndexNumber(String BufferNumber);//BufferNumber's Finding In IndexBuffer
+    public String findAllByIndexNumber(String BufferNumber);//BufferNumber's Finding In IndexBuffe
+    public List<IndexBuilder> updateByIndexBuffer(String IndexBuffer);//Updating IndxBuffer in Repo
+    public String  updateByIndexNumber(String BufferNumber);//BufferNumber's   Update 's  IndexBuffer
 
 }
