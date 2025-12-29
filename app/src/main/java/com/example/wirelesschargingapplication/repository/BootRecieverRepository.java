22a2e580296f7075f3e25fc404d10b67ba2e7660
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-12-29 11:56:07 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/BootRecieverRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/BootRecieverRepository.java
@@ -1,12 +1,13 @@
 package com.example.wirelesschargingapplication.repository;
 
-//WID(13/10/2025)
+//WID(29/12/2025)
 
 import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BootReciever;
 @Repository
 public  interface  BootRecieverRepository extends JpaRepsoitory<BootReciever,String>  {
  public String findAllByLogSize(Integer LOG_SIZE);
  public String saveAllByLogSize(Integer LOG_SIZE);
+ public String updateByLogSize(Integer LOG_SIZE);
  public String saveAllByTag(String Tag);
  public String findAllByTag(String Tag);
  public Strign saveAllByFIle(String File);
