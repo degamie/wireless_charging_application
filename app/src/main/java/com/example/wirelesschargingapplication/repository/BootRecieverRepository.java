87124757cf11b2383a43e4915b7ebba8b8d02edb
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-01-07 09:46:01 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/BootRecieverRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/BootRecieverRepository.java
@@ -1,24 +1,14 @@
 package com.example.wirelesschargingapplication.repository;
 
-//WID(1/1/2026 )
 
 import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BootReciever;
-
-import java.io.File;
-
 @Repository
 public  interface  BootRecieverRepository extends JpaRepsoitory<BootReciever,String>  {
  public String findAllByLogSize(Integer LOG_SIZE);
  public String saveAllByLogSize(Integer LOG_SIZE);
- public String updateByLogSize(Integer LOG_SIZE);
  public String saveAllByTag(String Tag);
  public String findAllByTag(String Tag);
- public String saveAllByFIle(String File);
- public String findAllByFIle(String File);
- public String updateByFile(File file);
-    public String findAllByTOMB_DIR(String TOMB_DIR);//Finding TOMB_DIR
- public String saveAllByTOMB_DIR(String TOMB_DIR);
- public String updaateByTOMB_DIR(String TOMB_DIR);
- public String existsByTOMB_DIR(String TOMB_DIR);
-
+ public String saveAllByBatteryLevel(Integer mBatteryLevel);
+ public String saveAllRecieverLine(String RecieverLine);
+public void updateByTag(String Tag);
 }
