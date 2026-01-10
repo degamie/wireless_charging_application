2d8c9b28b68761e544ff8f556d0541c2c24e6ef1
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-01-10 12:27:24 +0530

    Add files via upload

---
+++ BatteryWirelessRepository.java
@@ -0,0 +1,51 @@
+//WID(10/1/2026)(DegamieSign)
+package com.example.wirelesschargingapplication.repository;
+
+import static android.content.Context.USB_SERVICE;
+import static android.provider.SyncStateContract.Helpers.update;
+import static androidx.core.content.ContextCompat.getSystemService;
+import static androidx.core.content.ContextCompat.registerReceiver;
+
+import android.app.ApplicationErrorReport;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.BatteryState;
+import android.hardware.usb.UsbManager;
+import android.os.BatteryManager;
+import android.os.DropBoxManager;
+import android.provider.Settings;
+
+import com.example.wirelesschargingapplication.MainActivity;
+import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BatterySimulator.LED;
+import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BatterySimulator.UEventObserver.UeventObserver;
+import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BatterySimulator.UsbReciever.UsbReciever;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BatterySimulator.BatteryWireless;
+
+//public class BatteryWireless extends AppCompatActivity implements WifiBroadCastReciever.wifiChangeBroadCastLister{
+@Repository
+public interface BatteryWirelessRepository extends JpaRepository  <BatteryWireless,String>{
+    @Autowwired
+    public BatteryWireless batterywireless;
+    public String findAllByMplugType(Integer mPLugType);
+    public String saveAllByMplugType(Integer mPLugType);
+    public String updateAllByMpPlugType(Integer mPlugType);
+
+    public String findAllBYScale(int Scale);
+    public String saveAllByScale(int Scale);
+    public Integer savaAllBymAcOnLine(Integer mAcOnLine);
+    public String existsByMpPlugType(Integer mPlugType);//Checking mPlugType's existence in App
+    public String findAllByUsbReciever(UsbReciever usbReciever);
+    public String saveAllBYUsbReciever(UsbReciever usbReciever);
+    public String saveAllBymAcOnUsb(Integer mAcOnUsb);
+    public String findAllBymAcOnUsb(Integer mAcOnUsb);
+    public String saveAllByScale(Integer scale);
+    public void findAllByScale(Integer scale);
+    public String updateByScale(Integer scale);
+    public void existsByScale(Integer scale);
+    public void updateBymAcOnUsb(Integer mAcOnUsb);
+}
