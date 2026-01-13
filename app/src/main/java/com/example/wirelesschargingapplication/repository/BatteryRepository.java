597b6e3c7f492d761b62f4d7cc00d1da9ad63492
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-01-13 11:23:42 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/BatteryRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/BatteryRepository.java
@@ -1,5 +1,7 @@
+//WID(13/1/2026)(DegamieSign)(Sarthak Mittal)
 package  com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.Repository.BatteryRepository;
 
+import com.android.aapt.Resources;
 import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.model.Battery;
 @Repository
 public interface BatteryRepository extends JpaRepository<Battery,String>{
@@ -8,10 +10,6 @@ public interface BatteryRepository extends JpaRepository<Battery,String>{
     public String saveAllByBatteryId(String BatteryId);
     public String saveAllByBatteryName(String BatteryName);
     public String saveAllByBatteryPercentage(String BatteryPercentage);
-
-//dev by Sarthak Mittal(S
-Degamiesign)("10/08/2025")
-public String findAllByBatteryId(String Battery id);
-
+    public void   updateByBatteryName(Resources.String BatteryName);//Updating BatteryName in App
 
 }
