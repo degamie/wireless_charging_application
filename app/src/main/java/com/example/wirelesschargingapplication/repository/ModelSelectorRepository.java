cdc560e1cb182e9edf814e2272d93069188297da
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-01-20 15:52:48 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/ModelSelectorRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/ModelSelectorRepository.java
@@ -1,22 +1,23 @@
+//WID(20/12/2025)//Sarthak Mittal(Degamiesign)#1.1.1
 package com.example.axremulator2.ModelSelector;
-
-//WID(25/12/2025)//Sarthak Mittal(Degamiesign)
 import android.content.Context;
 
 import androidx.compose.runtime.Anchor;
 
+import com.android.aapt.Resources;
 import com.google.ar.core.ArCoreApk;
 import com.google.ar.core.Camera;
 import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.Camera;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.example.axremulator2.ModelSelector.XRSession;
 @Repository
 public  interface ModelSelectorRepository extends JpaRepository<ModelSelector,String> {
     @Autowired
     public ModelSelector modelSelector;
-
-    public String saveAllCameraSelector(Camera arcam);
-    public String findAllCameraSelector(Camera arcam);
+    public ModelSelector getModelSelector(ModelSelector modelSelector){return modelSelector;}//Fethcing MdlSelector in App
+    public Resources.String saveAllCameraSelector(Camera arcam);
+    public Resources.String findAllCameraSelector(Camera arcam);
     public String saveAllByModelMaterial(String ModelMaterial);
     public void findAllByModelMaterial(String ModelMaterial);//Binding ModelMaterial in App
     public String updateByModelMaterial(String ModelMaterial);//Updaating Modelmaterial In App
