d3d002c4c3667b44e840b00e0783067508ae9cd0
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-01-20 10:07:20 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/BootRecieverRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/BootRecieverRepository.java
@@ -1,14 +1,24 @@
+//WID(20/1/2026 )(Sarthak Mittal)(DegamieSign)#1
 package com.example.wirelesschargingapplication.repository;
-
-
+import com.android.aapt.Resources;
 import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BootReciever;
+
+import java.io.File;
+
 @Repository
-public  interface  BootRecieverRepository extends JpaRepsoitory<BootReciever,String>  {
+public  interface  BootRecieverRepository extends JpaRepsoitory<BootReciever, Resources.String>  {
  public String findAllByLogSize(Integer LOG_SIZE);
  public String saveAllByLogSize(Integer LOG_SIZE);
+ public String updateByLogSize(Integer LOG_SIZE);
+ public String existsByLogSize(int Log_Size);
  public String saveAllByTag(String Tag);
  public String findAllByTag(String Tag);
- public String saveAllByBatteryLevel(Integer mBatteryLevel);
- public String saveAllRecieverLine(String RecieverLine);
-public void updateByTag(String Tag);
+ public String saveAllByFIle(String File);
+ public String findAllByFIle(String File);
+ public String updateByFile(File file);
+    public Resources.String findAllByTOMB_DIR(String TOMB_DIR);//Finding TOMB_DIR
+ public String saveAllByTOMB_DIR(String TOMB_DIR);
+ public String updaateByTOMB_DIR(String TOMB_DIR);
+ public String existsByTOMB_DIR(String TOMB_DIR);
+
 }
