7f0ca8056ab505b9c03ef1942a46ab5cf6ace232
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-01-29 11:54:22 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/ModelSelectorRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/ModelSelectorRepository.java
@@ -1,4 +1,4 @@
-//WID(20/12/2025)//Sarthak Mittal(Degamiesign)#1.1.1
+//WID(29/12/2025)//Sarthak Mittal(Degamiesign)#1.1.1.q
 package com.example.axremulator2.ModelSelector;
 import android.content.Context;
 
@@ -15,10 +15,12 @@ import com.example.axremulator2.ModelSelector.XRSession;
 public  interface ModelSelectorRepository extends JpaRepository<ModelSelector,String> {
     @Autowired
     public ModelSelector modelSelector;
+    public void setModelSelector(ModelSelector modelSelector){this.modelSelector=modelSelector;)//Binding ModelSelector in App
     public ModelSelector getModelSelector(ModelSelector modelSelector){return modelSelector;}//Fethcing MdlSelector in App
     public Resources.String saveAllCameraSelector(Camera arcam);
     public Resources.String findAllCameraSelector(Camera arcam);
     public String saveAllByModelMaterial(String ModelMaterial);
     public void findAllByModelMaterial(String ModelMaterial);//Binding ModelMaterial in App
     public String updateByModelMaterial(String ModelMaterial);//Updaating Modelmaterial In App
+    public void existsByModelMaterial(String ModelMaterial);//Checking ModelMaterial in App
 }
