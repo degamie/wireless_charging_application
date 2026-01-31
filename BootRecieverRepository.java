5e6122280e50b2e9e9b57fe4fc858107b8563475
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-01-31 08:06:15 +0530

    Add files via upload

---
+++ BootRecieverRepository.java
@@ -0,0 +1,26 @@
+//WID(31/1/2026 )(Sarthak Mittal)(DegamieSign)#1.1
+package com.example.wirelesschargingapplication.repository;
+import com.android.aapt.Resources;
+import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BootReciever;
+
+import java.io.File;
+
+@Repository
+public  interface  BootRecieverRepository extends JpaRepsoitory<BootReciever, Resources.String>  {
+ public BootRecieverRepository bootrecieverrepository;
+ public BootRecieverRepository getBootrecieverrepository(BootRecieverRepository bootrecieverrepository){return bootrecieverrepository;}//Fetching BootReciever in App
+ public String findAllByLogSize(Integer LOG_SIZE);
+ public String saveAllByLogSize(Integer LOG_SIZE);
+ public String updateByLogSize(Integer LOG_SIZE);
+ public String existsByLogSize(int Log_Size);
+ public String saveAllByTag(String Tag);
+ public String findAllByTag(String Tag);
+ public String saveAllByFIle(String File);
+ public String findAllByFIle(String File);
+ public String updateByFile(File file);
+    public Resources.String findAllByTOMB_DIR(String TOMB_DIR);//Finding TOMB_DIR
+ public String saveAllByTOMB_DIR(String TOMB_DIR);
+ public String updaateByTOMB_DIR(String TOMB_DIR);
+ public String existsByTOMB_DIR(String TOMB_DIR);
+
+}
