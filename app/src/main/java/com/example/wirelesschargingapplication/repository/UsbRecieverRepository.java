aa1999e778d300265db6ef44a892ebc5e2e00950
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-02-05 08:15:40 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/UsbRecieverRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/UsbRecieverRepository.java
@@ -1,4 +1,4 @@
-//WID(4/02/2026)(Degamiesign)(Sarthak Mittal)#1.1.1
+//WID(5/02/2026)(Degamiesign)(Sarthak Mittal)#1.1.1.1
 import android.health.connect.datatypes.Device;
 import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BatterySimulator.UsbReciever.UsbReciever;
 
@@ -9,6 +9,7 @@ import java.util.List;
 @Repository
 public interface UsbRecieverRepository extends JpaRepository<UsbReciever,String> {
     public UsbRecieverRepository usbRecieverRepository;
+    public void updateusbRecieverRepository(UsbRecieverRepository usbRecieverRepository){getUsbRecieverRepository(usbRecieverRepository)+setUsbRecieverRepository(usbRecieverRepository)+1;}//Updating UsbReciever in App
     public void setUsbRecieverRepository(UsbRecieverRepository usbRecieverRepository){this.usbRecieverRepository=usbRecieverRepository;}//Binding usbRecieverRepository in App
     public UsbRecieverRepository getUsbRecieverRepository(UsbRecieverRepository usbRecieverRepository){return usbRecieverRepository;}//Fetching UsbRecieverRepo in App
     public List<UsbReciever> saveAll();
