91a1b56ab74b7d0914593f2dff7f9a875157d915
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-02-06 08:33:14 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/BatteryListenerRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/BatteryListenerRepository.java
@@ -1,4 +1,4 @@
-//WID(28/1/2026)Sarthak Mittal(Degamiesign)#1.1.1.1.1.1
+//WID(6/2/2026)Sarthak Mittal(Degamiesign)#1.1.1.1.1.1.1
 import java.util.List;
 
 import android.content.Context;
@@ -11,6 +11,7 @@ import jdk.internal.loader.Resource;
 @Repository
 public interface BatteryListenerRepository extends JpaRepository<batterylistner,String>{
     public BatteryListenerRepository batteryListenerRepository = null;
+    public BatteryListenerRepository getBatteryListenerRepository(BatteryListenerRepository batteryListenerRepository){return batteryListenerRepository;}//Fetching BatteryListener in App
     @Autowired
     public BatteryListener batterylistner;
     public void setBatterylistner(BatteryListener batterylistner){this.batterylistener=batterylistner;}//binding batteryListener in App
