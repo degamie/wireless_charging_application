4cde69f0c3d566b448a426a82b4a9babb9b904c8
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-02-18 12:54:34 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/BootRecieverRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/BootRecieverRepository.java
@@ -1,6 +1,7 @@
-//WID(25/1/2026 )(Sarthak Mittal)(DegamieSign)#1.1
+//WID(18/2/2026 )(Sarthak Mittal)(DegamieSign)#1.1.1
 package com.example.wirelesschargingapplication.repository;
 import com.android.aapt.Resources;
+import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BatteryRecieverRepository;
 import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BootReciever;
 
 import java.io.File;
@@ -8,6 +9,9 @@ import java.io.File;
 @Repository
 public  interface  BootRecieverRepository extends JpaRepsoitory<BootReciever, Resources.String>  {
  public BootRecieverRepository bootrecieverrepository;
+ public void updateByBootRecieverRepository(BootRecieverRepository bootrecieverrepository);
+ public void setBootrecieverrepository(BootRecieverRepository bootrecieverrepository){this.batteryRecieverRepository=batteryRecieverRepository;}
+ public BootRecieverRepository getBootrecieverrepository(BootRecieverRepository bootrecieverrepository){return bootrecieverrepository;}//Fetching BootReciever in App
  public String findAllByLogSize(Integer LOG_SIZE);
  public String saveAllByLogSize(Integer LOG_SIZE);
  public String updateByLogSize(Integer LOG_SIZE);
