3d70ca0989966f8247203768835260807cbaf759
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-02-18 19:25:34 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/WifiDetailsRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/WifiDetailsRepository.java
@@ -1,9 +1,10 @@
-//WID(17/2/2025)//Sarthak Mittal aka Degamiesign #1.1.1
+//WID(18/2/2025)//Sarthak Mittal aka Degamiesign #1.1.1.1
 import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.WifiDetails;
 
 @Repository
 public interface WifiDetailsRepository extends JpaRepository<WifiDetails,String>{
     public WifiDetailsRepository wifidetailsrepository;
+    public WifiDetailsRepository getWifidetailsrepository(WifiDetailsRepository wifidetailsrepository);
     public void findBynetwoekId(Integer networkId);
     public void findAllByNetworkSpeed(double networkSpeed);
 public double saveAllByNetworkSpeed(double networkSpeed);
