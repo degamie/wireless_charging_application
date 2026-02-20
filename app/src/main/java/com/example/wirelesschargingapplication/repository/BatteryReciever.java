de4fe1b9a03e4cf9cca0246e7335291b4a6661fb
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-02-20 13:19:47 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/BatteryReciever.java
@@ -0,0 +1,40 @@
+//WIP(20/2/2026)(Sarthak Mittal)#1
+package com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application;
+
+import static androidx.compose.ui.semantics.SemanticsPropertiesKt.setText;
+
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.BatteryState;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.TextView;
+
+import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BatterySimulator.BatteryWireless;
+import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BroadCastReciever;
+
+@Getter
+@Setter
+@AllParamsConstructor
+@NoParamsConstructor
+public class BatteryReciever  extends BroadCastReciever {//BatteryReciever class inherititing BroadCastReciever
+    @Id
+    @GeneratedValue(strategy=GENERATIONTYPE.IDENTITY)
+    @Column(unique=true,nullable=false)
+    public Context context;
+    @Column(unique=true,nullable=false)
+    public Integer mBatteryLevel;
+    public TextView textView;
+    @Column(unique=true,nullable=false)
+    public String  networkInfo=null;
+    @Column(unique=true,nullable=false)
+    public ConnectivityManager connectivityManager = null;
+    @Column(unique=true,nullable=false)
+    public BatteryReciever batteryReciever;
+    @Column(unique=true,nullable=false)
+    public String networkId;
+//    @Column(unique=true,nullable=false)
+//    public String setBatteryLevel(Integer mBatteryLevel){this.mBatteryLevel=mBatteryLevel;}//Binding MBatteryLevel
+
+
+    }
