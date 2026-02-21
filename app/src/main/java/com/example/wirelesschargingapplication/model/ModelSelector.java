ed36a53aad5c3c2fe961818ff6775a5d71b315bf
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-02-21 07:25:07 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/wirelesschargingapplication/model/ModelSelector.java
@@ -0,0 +1,13 @@
+//WIP(21/02/2026)(Sarthak Mittal)(Degamiesign)#1.1.1.1.1.1.1.1.1
+@Component
+public class Model{
+    public Model model;
+    public String mdlId;
+    public Model(String  mdlId){
+        this.mdlId=mdlId;
+    }
+
+
+
+
+}
