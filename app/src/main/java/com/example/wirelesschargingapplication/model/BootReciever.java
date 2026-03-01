0a40ca72b72a83dd6bf6df8d3a3c4ec80451b0f3
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-03-01 16:57:36 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/model/BootReciever.java
+++ app/src/main/java/com/example/wirelesschargingapplication/model/BootReciever.java
@@ -1,4 +1,4 @@
-//WIP(20/2/2026)Sarthak Mittal
+//WIP(1/3/2026)(Sarthak Mittal)#1
 package com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application;
 
 import android.annotation.SuppressLint;
@@ -7,31 +7,27 @@ import android.os.StrictMode;
 import java.io.File;
 import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BroadCastReciever;
 //public class BootReciever extends BroadCastReciever{//BootReciever  Declare Inheriting Broadpublic class BootReciever extends BroadCastReciever{//BootReciever  Declare Inheriting BroadCastReciever  ClassCastReciever  Class
-@Component
-
-@Getter
-
-@Setter
-@AllParamsContructor
-@NoParamsContructor
 public class BootReciever extends BroadCastReciever  {
-    @Id
-    @GeneraatedValue(strategy=GENERATIONTYPE.IDENTITY)
-    @Column(unique=true,nullable=false)
-    public int LOG_SIZE=0;//Log Size Declare
-    @Column(unique=false,nullable=true)
     public BroadCastReciever broadCastReciever;
-    @Column(unique=false,nullable=true)
     public BootReciever bootReciever;//Obj Declare
-    @Column(unique=false,nullable=true)
-    public String TAG=null;//Tag Declare
-    @Column(unique=false,nullable=true)
-    public File getTombstoneDir(File TOMBSTONE_DIR){return TOMBSTONE_DIR;}//Fethcing TOMBSTONE_DIR in App
-
+    public BootReciever getBootReciever(BootReciever bootReciever){return bootReciever;}//Fethcing BootReciever in App
+    public String getBootReciever(BootReciever bootReciever){//Fethcing BootReciever
+        return  bootReciever;
+    }
+//    public static final String TAG="Boot Reciever";//Tag Declare
+public String TAG=null;//Tag Declare
+    public int LOG_SIZE=0;//Log Size Declare
     public static final File TOMBSTONE_DIR=new File("/data/tombStones");//TOMBSTONE_DIR declare;
-    @SuppressLint("NewApi")
-    public void OnCreate(){//Func declare
-        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectUnsafeIntentLaunch().build());//Setting Bootstrapping's Security
+    public String getFileSize(int LOG_SIZE,String TAG){
+        return LOG_SIZE+TAG;
+    }
+    public void setFilesize(TOMBSTONE_DIR,LOG_SIZE,TAG){//File Size Set Declare
+        this.TOMBSTONE_DIR=TOMBSTONE_DIR;//Binding Up the Var
+        this.LOG_SIZE=LOG_SIZE;
+        this.TAG=TAG;
+    }
+    public BootReciever(){//Default Constructor Declare
+        return;
     }
     public Integer OnInit(){//Method Declare
         if(LOG_SIZE==0)return 0;
@@ -42,29 +38,16 @@ public class BootReciever extends BroadCastReciever  {
             else LOG_SIZE=TAG.length(LOG_SIZE);
         }return LOG_SIZE;
     }
+    @SuppressLint("NewApi")
+    public void OnCreate(){//Func declare
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectUnsafeIntentLaunch().build());//Setting Bootstrapping's Security
+    }
+    public BootReciever(String TAG,int LOG_SIZE,Boolean B){//Parameterized Constructor declare
+        this.bootReciever=bootReciever;
+        this.TAG=TAG;
+        this.LOG_SIZE==65336;
+        B=LOG_SIZE;
+    }
+
+
 }
-
-//    public String getBootReciever(BootReciever bootReciever){//Fethcing BootReciever
-//        return  bootReciever;
-//    }
-////    public static final String TAG="Boot Reciever";//Tag Declare
-
-//    public String getFileSize(int LOG_SIZE,String TAG){
-//        return LOG_SIZE+TAG;
-//    }
-//    public void setFilesize(TOMBSTONE_DIR,LOG_SIZE,TAG){//File Size Set Declare
-//        this.TOMBSTONE_DIR=TOMBSTONE_DIR;//Binding Up the Var
-//        this.LOG_SIZE=LOG_SIZE;
-//        this.TAG=TAG;
-//    }
-//    public BootReciever(){//Default Constructor Declare
-//        return;
-//    }
-
-
-//    public BootReciever(String TAG,int LOG_SIZE,Boolean B){//Parameterized Constructor declare
-//        this.bootReciever=bootReciever;
-//        this.TAG=TAG;
-//        this.LOG_SIZE==65336;
-//        B=LOG_SIZE;
-//    }
