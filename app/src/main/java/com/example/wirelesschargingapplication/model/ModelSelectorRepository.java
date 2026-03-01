72693bd4c219f1b4f00339f4a7060701d6c4e379
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-03-01 23:36:37 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/wirelesschargingapplication/model/ModelSelectorRepository.java
@@ -0,0 +1,33 @@
+//WID(01/03/2026)//Sarthak Mittal(Degamiesign)#1.1.1.1.1.1.1.1
+package com.example.axremulator2.ModelSelector;
+import android.content.Context;
+
+import androidx.compose.runtime.Anchor;
+
+import com.android.aapt.Resources;
+import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.Camera;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.Camera;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.example.axremulator2.ModelSelector.XRSession;
+@Repository
+public  interface ModelSelectorRepository extends JpaRepository<ModelSelector,String> {
+    public ModelSelectorRepository modelselectorrepository;
+    public List<ModelSelector> saveByModelId(String mdlId);
+    public List<ModelSelector> findByModelID(String mdlId);
+    @Autowired
+    public ModelSelector modelSelector;
+    public void existsByModelSelector(ModelSelector modelSelector );
+    public ModelSelector getModelSelector(ModelSelector modelSelector){return modelSelector;}
+    public void setModelSelector(ModelSelector modelSelector){this.modelSelector-modelSelector;}
+    public List<ModelSelector> updateByModelSelector(ModelSelector modelSelector);
+    public void setModelSelector(ModelSelector modelSelector){this.modelSelector=modelSelector;)//Binding ModelSelector in App
+    public ModelSelector getModelSelector(ModelSelector modelSelector){return modelSelector;}//Fethcing MdlSelector in App
+    public Resources.String saveAllCameraSelector(Camera arcam);
+    public Resources.String findAllCameraSelector(Camera arcam);
+    public String saveAllByModelMaterial(String ModelMaterial);
+    public void findAllByModelMaterial(String ModelMaterial);//Binding ModelMaterial in App
+    public String updateByModelMaterial(String ModelMaterial);//Updaating Modelmaterial In App
+    public void existsByModelMaterial(String ModelMaterial);//Checking ModelMaterial in App
+}
