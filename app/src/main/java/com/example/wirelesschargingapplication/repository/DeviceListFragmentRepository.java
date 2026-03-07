78f9e8b251015404fab9e8d2cbeebdf9a47728f1
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-03-07 10:08:24 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/DeviceListFragmentRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/DeviceListFragmentRepository.java
@@ -1,4 +1,4 @@
-//WID(03/03/2026)(DegamieSign)(Sarthak Mittal)#1.1.1.1.1.1
+//WID(07/03/2026)(DegamieSign)(Sarthak Mittal)#1.1.1.1.1.1.1
 import com.example.wirelesschargingapplication.DeviceListFragment;
 
 import java.util.List;
@@ -6,6 +6,7 @@ import java.util.List;
 @Repository
 public interface DeviceListRepsitory extends JpaRepository<DeviceListFragment,String> {  //Fetching and Retrieving All DeviceListFragment's Params
     public DeviceListFragment devicelistfragment = null;
+    public List<DeviceListFragment> findByDeviceListFragement(DeviceListFragment devicelistfragment);
     public void updateByDeviceListFragment(DeviceListFragment devicelistfragment);
     public List<DeviceListFragment> getDeviceListFragment(DeviceListFragment devicelistfragment);
     public   void setDeviceListFragement(DeviceListFragment devicelistfragment);
