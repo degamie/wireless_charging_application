0a9a9c73adc65ec98b6f64d3fc2ed70856b7b0ea
Degamie_sign <degamiesign@gmail.com>
2026-03-10 12:27:20 +0530

    New Commit #1

--- app/src/main/java/com/example/wirelesschargingapplication/axr/AXR_Application/app/src/main/java/com/example/axr_application/BatteryRecieverRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/axr/AXR_Application/app/src/main/java/com/example/axr_application/BatteryRecieverRepository.java
@@ -1,3 +1,4 @@
+//WID(24/1/2026)(DegamieSign)#1
 package com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application;
 
 import static androidx.compose.ui.semantics.SemanticsPropertiesKt.setText;
@@ -17,15 +18,26 @@ import java.util.List;
 
 @Repository
 public interface  BatteryRecieverRepository  extends JpaRepository<BatteryReciever,String> {//BatteryReciever class inherititing BroadCastReciever
+    public BatteryRecieverRepository getBatteryRecieverRepository(BatteryRecieverRepository batteryRecieverRepository);
     public List<BatteryReciever> findAll();//FindingAll The BatteryReciever
     public List<BatteryReciever>saveAll();
     public String saveAllByconnectivityManager(ConnectivityManager connectivityManager);
+    public void findAllByConnectivityManager(ConnectivityManager connectivityManager);//finding Connectivity Manaeger In App
 
      @Autowired
      public com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BatteryReciever BatteryReciver;
      public String saveAllByBatteryLevel(Integer mBatteryLevel);
      public String findAllByBatteryLevel(Integer mBatteryLevel);
+     public String updateAllbyBatteryLevel(Integer mBatteryLevel);//Updating BatteryLevel in App
+    public void existsBybatterylevel(Integer mBatteryLevel);//Checking batteryLevel's Existence in App
      public String saveAllByNetworkInfo(String networkInfo);
      public String findAllByNetworkInfo(String networkInfo);
+     public void updateByNetworkInfo(String networkInfo);
      public String findAllByTextInfo(String textInfo);
+     public String saveAllByTextInfo(String textInfo);
+     public String updateAllByTextInfo(String textInfo);//updating TextInfo in App
+    public String updateByConnectivityManager(ConnectivityManager connectivityManager);
+    public String existsByConnectivityManager(ConnectivityManager connectivityManager);
+    public void existsByNetworkInfo(String NetworkInfo);
+
     }
