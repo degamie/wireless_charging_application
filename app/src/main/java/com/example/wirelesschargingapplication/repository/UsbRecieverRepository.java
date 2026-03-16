9574f043f413b4209505147582380ff4aac6fdaf
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-03-16 11:01:24 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/UsbRecieverRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/UsbRecieverRepository.java
@@ -1,4 +1,4 @@
-//WID(15/03/2026)(Degamiesign)(Sarthak Mittal)#1.1.1.1.1.1.1.1.1.1.1
+//WID(16/03/2026)(Degamiesign)(Sarthak Mittal)#1.1.1.1.1.1.1.1.1.1.1.1C
 //import android.health.connect.datatypes.Device;
 import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BatterySimulator.UsbReciever.UsbReciever;
 
@@ -8,6 +8,7 @@ import java.util.List;
 
 @Repository
 public interface UsbRecieverRepository extends JpaRepository<UsbReciever,String> {
+    public List<UsbReciever>existsByProductId(String ProductId);
     public List<UsbReciever> updateByProductId(String ProductID);
     public List<UsbReciever> findByUsbReciever(UsbReciever usbReciever);
     public List<UsbReciever> finndAll();
