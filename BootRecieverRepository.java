ef88f1fecc43ee578dade0bfa00a440fafc22532
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-03-18 12:22:44 +0530

    Add files via upload

--- BootRecieverRepository.java
+++ BootRecieverRepository.java
@@ -1,14 +1,25 @@
-//WID(31/1/2026 )(Sarthak Mittal)(DegamieSign)#1.1
+//WID(18/3/2026 )(Sarthak Mittal)(DegamieSign)#1.1.1.1.1.1.1,1.1.1
 package com.example.wirelesschargingapplication.repository;
 import com.android.aapt.Resources;
-import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BatteryRecieverRepository;
 import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BootReciever;
+//import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BatteryRecieverRepository;
+//import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BootReciever;
 
 import java.io.File;
+import java.util.List;
 
 @Repository
 public  interface  BootRecieverRepository extends JpaRepsoitory<BootReciever, Resources.String>  {
+    public List<BootReciever> finndByfileName(String fileName);
+ public List<BootReciever> updateByBootReciever();
+    public List<BootReciever> saveByBootRecieverRepository;
  public BootRecieverRepository bootrecieverrepository;
+ public List<BootReciever> findByBootRecieverRepository(BootRecieverRepository bootrecieverrepository);
+ public void setBootRecieverRepository(BootRecieverRepository bootrecieverrepository);
+ public List<BootReciever> saveByBootReciever(BootReciever bootReciever);
+ public List<BootReciever> findByBootReciever(BootReciever bootReciever);
+ public BootReciever existsByBootReciever(BootReciever bootReciever);
+ public void updateByBootRecieverRepository(BootRecieverRepository bootrecieverrepository);
  public void setBootrecieverrepository(BootRecieverRepository bootrecieverrepository){this.batteryRecieverRepository=batteryRecieverRepository;}
  public BootRecieverRepository getBootrecieverrepository(BootRecieverRepository bootrecieverrepository){return bootrecieverrepository;}//Fetching BootReciever in App
  public String findAllByLogSize(Integer LOG_SIZE);
