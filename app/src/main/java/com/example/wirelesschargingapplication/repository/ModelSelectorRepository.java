67e7c4efd656bd7c88b12360643b9213614f15dd
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-03-28 12:35:02 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/ModelSelectorRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/ModelSelectorRepository.java
@@ -1,4 +1,4 @@
-//WID(17/03C/2026)//Sarthak Mittal(Degamiesign)#1.1.1.1.1.1.1.1.1.1.1
+//WID(28/03C/2026)//Sarthak Mittal(Degamiesign)#1.1.1.1.1.1.1.1.1.1.1.1
 package com.example.wirelesschargingapplication.repository;
 import android.content.Context;
 
@@ -17,6 +17,7 @@ import java.util.List;
 
 @Repository
 public  interface ModelSelectorRepository extends JpaRepository<ModelSelector,String> {
+    public List<ModelSelector> saveByModelSelector(ModelSelector modelSelector);
     public List<ModelSelector> findBymodelSelector(ModelSelector modelSelector);
 //}
     public List<ModelSelector>updateByModelId(String mdlId);//updating ModelId in Appp
