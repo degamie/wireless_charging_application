d737c81962cc52406c74871032e7304d5ed72ee7
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-04-19 11:10:20 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/DeviceListFragmentRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/DeviceListFragmentRepository.java
@@ -1,34 +1,12 @@
-//WID(17/04/2026)(DegamieSign)(Sarthak Mittal)#1.1.1.1.1.1.1.1C.1c.1.1.1.1.1c.1s
+//WID(19/04/2026)(Sarthak Mittal)(DegamieSign)#1.1.1
 import com.example.wirelesschargingapplication.DeviceListFragment;
 
 import java.util.List;
 
 @Repository
-public interface DeviceListRepsitory extends JpaRepository<DeviceListFragment,String> {  //Fetching and Retrieving All DeviceListFragment's Params
-    public List<DeviceListFragment>existsByPortNum(String PortNum);
-    public List<DeviceListFragment>udpateByPortNum(String PortNum);
-    public List<DeviceListFragment> saveByPortNum(String PortNum);
-    public List<DeviceListFragment> findByportNum (String portNum);
-//    public
-    public List<DeviceListFragment> existsByDeviceName(String DeviceName);
-    public List<DeviceListFragment> savebyDevicelIstFragment(DeviceListFragment devicelistfragments);
-    public DeviceListFragment devicelistfragment = null;
-    public List<DeviceListFragment> findByDeviceListFragement(DeviceListFragment devicelistfragment);
-    public void updateByDeviceListFragment(DeviceListFragment devicelistfragment);
-    public List<DeviceListFragment> getDeviceListFragment(DeviceListFragment devicelistfragment);
-    public   void setDeviceListFragement(DeviceListFragment devicelistfragment);
-    public List<DeviceListFragment>saveByDeviceListFragment(DeviceListFragment devicelistfragment);
-    public List<DeviceListFragment> findByDeviceListFragment(DeviceListFragment devicelistfragment);
+public interface DeviceListRepsitory extends JpaRepository<DeviceListFragment,String> {
+    public List<DeviceListFragment> updateByfragementId(String fragmentId);
+    public List<DeviceListFragment>saveByfragmentId(String fragmentId);
+    public List<DeviceListFragment>findByfraagmnetId(String fragmentId);
     public List<DeviceListFragment> saveAll();
-    public String findAll();
-    public String findAlldserialById(String serialId);
-    public String saveAlldserialById(String serialId);
-    public String saveAllByDeviceName(String DeviceName);
-    public void updateByDeviceName(String DeviceName);//Updating DeviceName in App
-    public void existsByPortNum(String port);
-    public String saveAllByPort(String Port);
-    public void findAllByPort(String port);
-    public String updateByPort(String port);
-    public void findByDeviceName(String  DeviceName);
-    public String saveByDeviceName(String DeviceName);
 }
