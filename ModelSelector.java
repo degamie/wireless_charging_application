d566932016829aff0f55d4a14648157006957bf9
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-04-27 07:27:10 +0530

    Add files via upload

---
+++ ModelSelector.java
@@ -0,0 +1,7 @@
+//WID(27/04/2026)(Sarthak Mittal)(DegamieSign)#1.1
+package com.example.wirelesschargingapplication.model.UsbReciever;
+
+public class ModelSelector {
+    public String modelName;
+    public String modelId;
+}
