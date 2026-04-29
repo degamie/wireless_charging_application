7cc6f13d109689dd61c05c28f53941695be2b789
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-04-29 11:13:27 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/BatteryListenerRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/BatteryListenerRepository.java
@@ -1,4 +1,4 @@
-//WID(27/04/2026)(Sarthak Mittal)#1.1.1.1.1.1.1.1/.1/1.1/1/1.1
+//WID(29/04/2026)(Sarthak Mittal)#1.1.1.1.1.1.1.1/.1/1.1/1/1.1/1
 package com.example.wirelesschargingapplication.repository;//WID(28/2/2026)Sarthak Mittal(Degamiesign)#1.1.1.1.1.1.1.1.1.1
 import java.util.List;
 
@@ -12,7 +12,8 @@ import com.example.wirelesschargingapplication.model.BattteryListener;
 
 @Repository
 public interface BatteryListenerRepository extends JpaRepository<batterylistner,String>{
-    public List<BattteryListener>saveByBatteryName(String batteryName);
+    public List<BatteryListener>updateByBatteryName(String batteryName);
+     public List<BattteryListener>saveByBatteryName(String batteryName);
     public List<BattteryListener>findByBatteryName(String batteryName);
     public void existsByIntent(Intent intent);
     public List<BatteryListener>updateByIntent(Intent intent);
