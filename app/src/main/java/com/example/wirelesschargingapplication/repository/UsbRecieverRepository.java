28f54d6e6f8cae6a76d46a8a2e0f78f272ba8f0b
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-05-07 22:18:56 +0530

    Add files via upload

--- app/src/main/java/com/example/wirelesschargingapplication/repository/UsbRecieverRepository.java
+++ app/src/main/java/com/example/wirelesschargingapplication/repository/UsbRecieverRepository.java
@@ -1,4 +1,4 @@
-//WID(27/04/2026)(Degamiesign)(Sarthak Mittal)#1.1.1.1.1.1.1.1.1.1.1.1C.1.1.1.1.1.1.1.1C.1.1/1
+//WID(7/05/2026)(Degamiesign)(Sarthak Mittal)#1.1.1.1.1.1.1.1.1.1.1.1C.1.1.1.1.1.1.1.1C.1.1/1.1
 //import android.health.connect.datatypes.Device;
 import android.bluetooth.BluetoothClass;
 import android.health.connect.datatypes.Device;
@@ -11,6 +11,7 @@ import java.util.List;
 
 @Repository
 public interface UsbRecieverRepository extends JpaRepository<UsbReciever,String> {
+    public List<UsbReciever>updatebyDevice(Device device);
     public List<UsbReciever>existsByCameraPID(String CameraPID);
     public void existsByUsbDriver(BluetoothClass.Device UsbDriver);//Checking UsbDriver's Existence in App
     public void updateByUsbDriver(BluetoothClass.Device UsbDriver);
