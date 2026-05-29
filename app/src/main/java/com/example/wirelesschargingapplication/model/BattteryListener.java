7e625ac7ebd1df8096d0e6da5ebd6a23370bb7e6
Degamie_sign <degamiesign@gmail.com>
2026-05-29 10:12:15 +0530

    Updating DeviceListFragment#1

---
+++ app/src/main/java/com/example/wirelesschargingapplication/model/BattteryListener.java
@@ -0,0 +1,4 @@
+package com.example.wirelesschargingapplication.model;
+
+public class BattteryListener {
+}
---
+++ app/src/main/java/com/example/wirelesschargingapplication/model/UsbReciever/ModelSelector.java
@@ -0,0 +1,4 @@
+package com.example.wirelesschargingapplication.model.UsbReciever;
+
+public class ModelSelector {
+}
